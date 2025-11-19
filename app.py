5287d18c64ddcde39eda98894a25a3f2a0dce8c2
Shivam Dayanand Swami <167025692+Shivam140902@users.noreply.github.com>
2025-11-20 00:43:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def get_db():
     """Return a connection to the SQLite DB (cached on flask.g)."""
     db = g.get("_sqlite_db", None)
     if db is None:
-        conn = sqlite3.connect(SQLITE_DB_PATH, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES, check_same_thread=False)
+        conn = sqlite3.connect(SQLITE_DB, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES, check_same_thread=False)
         conn.row_factory = sqlite3.Row
         g._sqlite_db = conn
         db = conn
